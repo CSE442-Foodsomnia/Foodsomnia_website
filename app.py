a67427d6e030a8ebbbdd6a8b81a5e2e26f63791a
David <dzhou7032@bths.edu>
2021-10-06 14:42:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ import sys
 
 if __name__ == "__main__":
     port = int(sys.argv[1]) if len(sys.argv) > 1 else 5000
-
+    print(port)
     app = init_app()
     app.run(debug=True, host='0.0.0.0', port=port)
