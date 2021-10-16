d4df6ea1ce0ea95aecac68991f28cc63415832a8
David <dzhou7032@bths.edu>
2021-10-15 21:36:49 -0400

    Added disliked page #49

--- web_app/food.py
+++ web_app/food.py
@@ -5,11 +5,12 @@ from random import randrange
 from . import db
 from .models import Recipe, Liked, Disliked
 from flask_login import login_user, logout_user, login_required, current_user
-
+import pandas as pd
 
 food = Blueprint("food", __name__)
 
 
+# we be forgettin comments out here
 @food.route("/food_rec", methods=['GET', 'POST'])
 @login_required
 def food_recommendation():
@@ -17,17 +18,15 @@ def food_recommendation():
     Function:
     Search through DB Liked table and recipe table then recommend a recipe that is similar
     """
-
     recipe_list = Recipe.query.all()
 
     i = randrange(0, len(recipe_list))
     random_recipe = recipe_list[i]
 
     if request.method == 'POST':
-
         key_pressed = request.get_json()["key_pressed"]
         user_id = current_user.id
-        recipe_id = random_recipe.id
+        recipe_id = request.get_json()["displayedrecipe"]
 
         if key_pressed == 'left':
             new_dislike = Disliked(user_id, recipe_id)
@@ -45,4 +44,27 @@ def food_recommendation():
         else:
             print("wrong key pressed")
 
-    return render_template('swipe.html', recipe=random_recipe)
+    return render_template('swipe.html', recipe=random_recipe, displayedid=random_recipe.id)
+
+
+@food.route("/trending")
+def trending():
+    return render_template('home.html')
+
+
+@food.route("/liked")
+def liked():
+    all_liked = pd.Series(Liked.query.filter_by(user_id=current_user.id))
+    recipe_list = [value.recipe_id for index, value in all_liked.items()]
+    recipe_query = Recipe.query.filter(Recipe.id.in_(recipe_list))
+
+    return render_template('liked.html', liked=recipe_query)
+
+
+@food.route("/disliked")
+def disliked():
+    all_disliked = pd.Series(Disliked.query.filter_by(user_id=current_user.id))
+    recipe_list = [value.recipe_id for index, value in all_disliked.items()]
+    recipe_query = Recipe.query.filter(Recipe.id.in_(recipe_list))
+
+    return render_template('disliked.html', disliked=recipe_query)
